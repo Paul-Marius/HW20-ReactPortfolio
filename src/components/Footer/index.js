70cabb7066156c8544a2eaad005b191bb17cb406
Paul Besliu <besliu_marius@yahoo.com>
2021-02-12 12:31:03 -0800

    first commit

---
+++ src/components/Footer/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+    return (
+        <div>
+
+            <footer className="footer">
+
+                <span class="c-copy-date">© 2021</span>
+                    Advance Space Remodeling. 
+
+                    <span class="c-copy-date">© 2021</span>
+                         All rights reserved.
+
+            </footer>
+
+        </div>
+    );
+}
+
+export default Footer;
+
